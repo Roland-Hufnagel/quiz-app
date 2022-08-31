5c0196e7a76d17fa2eaa44e1b4f76e873d213b27
Roland-Hufnagel <roland.hufnagel@gmx.de>
2022-08-31 20:51:27 +0200

    add js functionality to first card

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+const answerButton = document.querySelector('[data-js="answer-button"]');
 
+answerButton.addEventListener("click", () => {
+  const answer = document.querySelector('[data-js="answer"]');
+  
+  answer.classList.toggle("card__answer--off");
+
+  answerButton.textContent == "Hide Answer"
+    ? (answerButton.textContent = "Show Answer")
+    : (answerButton.textContent = "Hide Answer");
+});
